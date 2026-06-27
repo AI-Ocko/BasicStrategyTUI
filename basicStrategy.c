d3e34611da4a4df4b5454b544ed3177cf5602cc2
Christopher Gulsby <ocko13@icloud.com>
2026-06-26 19:05:39 -0700

    Created helper function to call trainer functions in main

--- basicStrategy.c
+++ basicStrategy.c
@@ -3,6 +3,18 @@
 #include <stdlib.h>
 #include <time.h>
 
+void Trainer(int (*trainerFunction)(Score *score)) {
+  Score score = {0, 0};
+  srand(time(NULL));
+  while (trainerFunction(&score)) {
+    if (score.total > 0) {
+      printf("\n--- Results ---\n");
+      printf("Score: %d / %d\n", score.correct, score.total);
+      printf("Accuracy: %.1f%%\n\n", (float)score.correct / score.total * 100);
+    }
+  };
+}
+
 int main(void) {
   char menu_option, initials;
   int difficulty;
@@ -11,47 +23,23 @@ int main(void) {
 
   do {
     printf("Main Menu\n");
-    printf("1. Pair Splitting");
-    printf("2. Soft Totals");
-    printf("3. Hard Totals");
+    printf("1. Pair Splitting\n");
+    printf("2. Soft Totals\n");
+    printf("3. Hard Totals\n");
+    printf("4. Options\n");
     printf("0. Exit\n");
     printf("Please enter an option from the main menu: ");
     scanf(" %c", &menu_option);
 
     switch (menu_option) {
     case '1':
-      Score score = {0, 0};
-      srand(time(NULL));
-      while (pairSplittingTrainer(&score)) {
-        if (score.total > 0) {
-          printf("\n--- Results ---\n");
-          printf("Score: %d / %d\n", score.correct, score.total);
-          printf("Accuracy: %.1f%%\n\n",
-                 (float)score.correct / score.total * 100);
-        }
-      };
+      Trainer(pairSplittingTrainer);
       break;
     case '2':
-      srand(time(NULL));
-      while (softTotalTrainer(&score)) {
-        if (score.total > 0) {
-          printf("\n--- Results ---\n");
-          printf("Score: %d / %d\n", score.correct, score.total);
-          printf("Accuracy: %.1f%%\n\n",
-                 (float)score.correct / score.total * 100);
-        }
-      };
+      Trainer(softTotalTrainer);
       break;
     case '3':
-      srand(time(NULL));
-      while (hardTotalTrainer(&score)) {
-        if (score.total > 0) {
-          printf("\n--- Results ---\n");
-          printf("Score: %d / %d\n", score.correct, score.total);
-          printf("Accuracy: %.1f%%\n\n",
-                 (float)score.correct / score.total * 100);
-        }
-      };
+      Trainer(hardTotalTrainer);
       break;
     case '0':
       break;
